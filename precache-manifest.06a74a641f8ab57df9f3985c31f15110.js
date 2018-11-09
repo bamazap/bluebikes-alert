e8889deb8ee0568a24e1c6b2dc75329d90789dfd
Barry Anthony McNamara III <bamazap@hotmail.com>
2018-11-08 23:49:11 -0500

    publish: Fixes #1
    generated from commit 54e3ee1d2c11d2b5a0f8fea5a84ec0ea75ef14ba

--- precache-manifest.06a74a641f8ab57df9f3985c31f15110.js
+++ precache-manifest.06a74a641f8ab57df9f3985c31f15110.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "a67004fd544f4a331f31",
+    "revision": "cf10bfb359700673b136",
     "url": "/static/css/main.7b7af7b5.chunk.css"
   },
   {
-    "revision": "a67004fd544f4a331f31",
-    "url": "/static/js/main.a67004fd.chunk.js"
+    "revision": "cf10bfb359700673b136",
+    "url": "/static/js/main.cf10bfb3.chunk.js"
   },
   {
     "revision": "5043d5581a0518e17708",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "38c30a69716079e3997610243598d70f",
+    "revision": "c30735db64df9dead83a480807cb9b7e",
     "url": "/index.html"
   }
 ];
